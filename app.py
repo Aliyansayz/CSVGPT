004fe8f0176b2a90744b644e2ef8c4d9f7671ca6
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-11-01 21:19:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from utils import query_agent
 from utils import init_llm
 
 
+
 if 'OpenAI_API_Key' not in st.session_state:
     st.session_state['OpenAI_API_Key'] =''
     
@@ -19,17 +20,18 @@ if done and st.session_state['OpenAI_API_Key'] !="":
     #Proceed only if API keys are provided
 
         llm = init_llm(key= st.session_state['OpenAI_API_Key'] )
-        if llm :
-            
-            st.header("Please upload your CSV file here:")
-            
-            # Capture the CSV file
-            data = st.file_uploader("Upload CSV file",type="csv")
-            
-            query = st.text_area("Enter your query")
-            button = st.button("Generate Response")
-        
-            if button :
-                # Get Response
-                answer =  query_agent(data=data, query=query, llm=llm )
-                st.write(answer)
+    
+if llm :
+    
+    st.header("Please upload your CSV file here:")
+    
+    # Capture the CSV file
+    data = st.file_uploader("Upload CSV file",type="csv")
+    
+    query = st.text_area("Enter your query")
+    button = st.button("Generate Response")
+
+    if button :
+        # Get Response
+        answer =  query_agent(data=data, query=query, llm=llm )
+        st.write(answer)
