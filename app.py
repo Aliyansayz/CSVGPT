40d46ba6e2b3db4e640fac274fbe3f49f0e289f8
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-11-01 21:43:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from utils import query_agent
-
+import asyncio
 
 
 if 'OpenAI_API_Key' not in st.session_state:
@@ -14,21 +14,21 @@ st.session_state['OpenAI_API_Key']= st.sidebar.text_input("What's your OpenAI AP
 
 done = st.sidebar.button("Add API key", key="load_button")
 
-
-if done and st.session_state['OpenAI_API_Key'] !="":
-    #Proceed only if API keys are provided
-
-
+async def run_app( key=st.session_state['OpenAI_API_Key'] ):
+    if done and key !="":
+        #Proceed only if API keys are provided
     
-    st.header("Please upload your CSV file here:")
     
-    # Capture the CSV file
-    data = st.file_uploader("Upload CSV file", type="csv")
-    
-    query = st.text_area("Enter your query")
-    button = st.button("Generate Response")
-    
-    if button :
-        # Get Response
-        answer =  query_agent(data=data, query=query, key= st.session_state['OpenAI_API_Key']  )
-        st.write(answer)
+        
+        await st.header("Please upload your CSV file here:")
+        
+        # Capture the CSV file
+        data = await st.file_uploader("Upload CSV file", type="csv")
+        
+        query = await st.text_area("Enter your query")
+        button = await st.button("Generate Response")
+        
+        if button :
+            # Get Response
+            answer = await  query_agent(data=data, query=query, key= st.session_state['OpenAI_API_Key']  )
+            await st.write(answer)
