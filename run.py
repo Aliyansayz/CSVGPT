9324d532a08ab8f3b20f39583034262f686730ce
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-11-02 16:26:24 +0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from langchain.llms import OpenAI
+import pandas as pd
+from langchain_experimental.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
+
+
+st.title('🦜🔗 CSV LLM App')
+
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
+def generate_response(input_text, data):
+
+    df = pd.read_csv(data) 
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    agent = create_pandas_dataframe_agent(llm, df, verbose=True)
+    st.info(agent.run(input_text))
+#   st.info(llm(input_text))
+
+with st.form('my_form'):
+    
+  data =  st.file_uploader("Upload CSV file", type="csv")
+  text = st.text_area('')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text, data)
