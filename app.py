55a6c4c4ddf688282d46d8bcdf22465e14a45e24
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-11-14 17:07:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from dotenv import load_dotenv
+
 from langchain.llms import OpenAI
 import pandas as pd
 from langchain_experimental.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
@@ -6,22 +8,23 @@ from langchain_experimental.agents.agent_toolkits.pandas.base import create_pand
 
 st.title('Analyzer App')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+# openai_api_key = st.sidebar.text_input('OpenAI API Key')
+# OpenAI (openai_api_key=key)
 
 def generate_response(input_text, data, key):
 
+    load_dotenv()
     df = pd.read_csv(data) 
-    llm = OpenAI( openai_api_key=key)
+    llm = OpenAI( )
     agent = create_pandas_dataframe_agent(llm, df, verbose=True)
     st.info(agent.run(input_text))
 
 
 with st.form('my_form'):
     
-  data =  st.file_uploader("Upload CSV file", type="csv")
+  data =  st.file_uploader("Upload CSV file :", type="csv")
   text = st.text_area('')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text, data, openai_api_key)
+  submitted = st.form_submit_button('Analyze CSV')
+  
+  if submitted :
+    generate_response(text, data )
